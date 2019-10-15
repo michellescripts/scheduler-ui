08b8958c16e69b7b3e8946431c5d5bcf837ed3b1
Michelle Bergquist <michelle.bergquist@developdenver.org>
2019-10-14 19:50:17 -0600

    Add Auth0 to the UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import './App.css';
+import NavBar from "./nav/NavBar";
+import { useAuth0 } from "./react-auth0-wrapper";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import PrivateRoute from "./components/PrivateRoute";
 
-class App extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <h3>Develop Denver 2020</h3>
-                <h1>Volunteers needed soon!</h1>
-            </React.Fragment>
-        )
-    }
+function App() {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return (
+      <div>Loading...</div>
+    );
+  }
+
+  return (
+      <React.Fragment>
+        <BrowserRouter>
+         <header>
+           <NavBar />
+         </header>
+         <Switch>
+           <Route path="/" exact />
+           <PrivateRoute path="/profile" component={Profile} />
+         </Switch>
+        </BrowserRouter>
+      </React.Fragment>
+  );
 }
 
 export default App;
